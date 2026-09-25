02b15af8cc21d50b78fcb8017a994964b6e81ffa
Artem Mikhailov <@email>
2026-09-25 14:29:13 +0200

    chore: configure separate .next-dev build directory for development

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: __dirname,
+  distDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
 };
 
 module.exports = nextConfig;
